1b67ced29e315f1e2f34b2fa3737700f2d7a44b6
unknown <Administrador@CETEM-2580.mineral.cetem>
2021-04-19 15:29:02 -0300

    Atividade finalizada

---
+++ semana3/variaveis/index.js
@@ -0,0 +1,125 @@
+//Exercícios de interpretação de código
+//Exercício 1
+//Será impresso 10 e depois 10,5
+
+//Exercício 2
+//Será impresso 10,10,10
+
+
+
+
+//Exercícios de escrita de código
+//Exercício 1
+/*
+const nome =""
+const idade = 0
+typeof nome 
+typeof idade 
+
+console.log(typeof nome, typeof idade) */
+
+//Impresso string e number, pois está atribuído "" que siginifica que entrará uma 
+// string e o valo 0 (zero) que idenficará que será atribuído um número
+
+/*
+const nome=prompt("Qual o seu nome?")
+const idade=prompt("Qual a sua idade?")
+typeof nome 
+typeof idade 
+
+console.log(typeof nome, typeof idade)
+
+//Impresso string e string
+
+console.log(nome, idade)
+//Impresso string e string e Fabricia 35
+
+console.log("Olá" + nome +  "você tem" + idade +  "anos. Onde nome e idade são os valores que o usuário inseriu")
+*/
+
+//Imprime os valores digitados pelo usuário, mas falta espaço entre os valores e a frase.
+
+
+
+//Exercício 2
+/*
+const nome= prompt("1.Qual o seu nome?")
+const idade= prompt("2.Qual a sua idade?")
+const cor= prompt("3.Qual a sua cor preferida?")
+const comida= prompt("4.Qual o seu prato predileto?")
+const endereco= prompt("5.Qual o seu endereço?")
+
+
+console.log ("1.Qual o seu nome?")
+console.log("Resposta:" + nome)
+console.log("")
+console.log ("2.Qual a sua idade?")
+console.log("Resposta:" + idade)
+console.log("")
+console.log ("3.Qual a sua cor preferida?")
+console.log("Resposta:" + cor)
+console.log("")
+console.log ("4.Qual o seu prato predileto?")
+console.log("Resposta:" + comida)
+console.log("")
+console.log ("5.Qual o seu endereço?")
+console.log("Resposta:" + endereco)  
+*/
+
+
+//Exercício 3
+//a
+/*
+var comida=['churrasco', 'feijoada', 'massa', 'moqueca de peixe', 'bobó de camarão']
+console.log (comida)
+*/
+
+//b
+/*
+var comida=['churrasco', 'feijoada', 'massa', 'moqueca de peixe', 'bobó de camarão']
+console.log ("Essas são as minhas comidas preferidas")
+console.log(comida[0])
+console.log(comida[1])
+console.log(comida[2])
+console.log(comida[3])
+console.log(comida[4])
+*/
+
+//c
+/*
+var preferida=['churrasco', 'feijoada', 'massa', 'moqueca de peixe', 'bobó de camarão']
+
+const comida=prompt("Qual a sua comida preferida?")
+
+preferida.splice(1, 0, comida);
+
+console.log(preferida)
+*/
+
+
+//Exercício 4
+/*
+var perguntas = ["Você gosta de viajar?","Você gosta pagode?","Você gosta feijão por cima do arroz?"]
+
+ var respostas= ['Verdadeiro', 'Verdadeiro', 'Verdadeiro']
+
+ console.log(perguntas[0],respostas[0])
+ console.log(perguntas[1],respostas[1])
+ console.log(perguntas[2],respostas[2])
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
