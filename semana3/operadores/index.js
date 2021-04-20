e85fd74c542e2aa734e84a1c1f908362ed67e477
unknown <Administrador@CETEM-2580.mineral.cetem>
2021-04-20 14:57:41 -0300

    Exercícios finalizados

---
+++ semana3/operadores/index.js
@@ -0,0 +1,71 @@
+// **Exercícios de interpretação de código**
+//1
+//a. false
+//b. false
+//c. true
+//e. indefinido
+
+
+//2
+//a. Sem dfinicição
+//b. null
+//c. 11
+//d. 3
+//e. [3,19, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+//f. 9
+
+
+//Exercício de escrita de código
+/*1
+const idade1= prompt("Qual a sua idade?")
+const idade2= prompt("Qual a idade do seu melhor amigo ou amiga?")
+const idade = Number(idade1) > Number(idade2)
+const diferenca = Number(idade1) - Number(idade2)
+console.log ("Sua idade é maior do que a do seu melhor amigo? " + idade)
+console.log ("A diferença de idade é de "  + diferenca +  " anos")
+*/
+
+//2
+/*
+const par= prompt("Por favor insira um número par")
+const numero = Number (par)%(2)
+console.log("O resto da operação é " +numero)
+
+Sempre aparece resto 0 (zero) quando é numero par
+Aparece o resto diferente de 0 quando é número ímpar */
+
+
+//3
+/*
+const array=[]
+const tarefa1= prompt("Qual a sua primeira tarefa do dia?")
+const tarefa2= prompt ("Qual a sua segunda tarefa do dia?")
+const tarefa3= prompt("Qual a sua terceira tarefa do dia?")
+array.push (tarefa1)
+array.push (tarefa2)
+array.push (tarefa3)
+console.log(array)
+const indice=prompt("Escolha um dos indices das tarefas, 0, 1 ou 2")
+array.splice(1,indice)
+console.log("Sua lista ficou assim " + array)
+*/
+
+//4
+/*
+const nomedousuario= prompt ("Digite seu nome")
+const email= prompt ("Digite seu e-mail")
+
+console.log ("O e-mail " +email+ " foi cadastrado com sucesso. Seja bem-vinda(o), " +nomedousuario+" !")
+
+alert ("O e-mail " +email+ " foi cadastrado com sucesso. Seja bem-vinda(o), " +nomedousuario+" !")
+*/
+
+
+
+
+
+
+
+
+
+
