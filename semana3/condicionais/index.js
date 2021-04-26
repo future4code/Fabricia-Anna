98c43ba00223c729863cdc4a339e9ca610a60eb6
unknown <Administrador@CETEM-2580.mineral.cetem>
2021-04-26 10:41:34 -0300

    Tentei finalizar o desafio

--- semana3/condicionais/index.js
+++ semana3/condicionais/index.js
@@ -88,7 +88,218 @@ console.log("Bom filme!")
 else {
   console.log("Escolha outro filme")
 }
+
+DESAFIO 1
+
+const filme= prompt("Qual o gênero de filme irão assistir?")
+const valor= Number (prompt("Qual o valor do ingresso?"))
+const valorformatado= valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+const snack=prompt("Qual lanche você vai comprar?")
+
+alert("O gênero escolhido foi " +filme+ " e o valor é "+valorformatado)
+
+const resposta=prompt("Aceitam assistir o filme? Sim ou não?")
+
+if (resposta.toLocaleLowerCase()=="sim")
+{
+console.log("Bom filme! \n ...com "  +snack)
+}
+else {
+  console.log("Escolha outro filme")
+}
+
+DESAFIO 2
 */
+const nomecompleto= prompt("Qual o seu nome completo?")
+const tipojogo= prompt("Esolha o tipo de jogo: IN para internacional ou \nDO para doméstico")
+const etapajogo= prompt("Insira a etapa do jogo: SF-semi-final; \n DT-decisão de terceiro lugar ou \n FI-final")
+const categoriajogo= Number (prompt("Qual categoria, 1, 2, 3 ou 4?"))
+const quantingresso= Number(prompt("Quantos ingressos?"))
+let valor = Number
+let valor1 = Number 
+let valor2 = Number 
+let valor3 = Number 
+let valor4 = Number 
+let totalingresso = Number
+let totalingressoin = Number
+
+
+
+//DOMESTICO
+if (tipojogo.toLocaleLowerCase()=="do" && etapajogo.toLocaleLowerCase()=="sf" && categoriajogo == 1)
+{
+  valor=1320
+  valor1 = 1320*quantingresso
+  let totalingresso = valor1.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+  console.log( " --- Dados da compra --- \n Nome do cliente: "+nomecompleto +"\n Tipo do jogo: "+tipojogo+ "\n Etapa do Jogo: "+etapajogo+"\n Categoria: "+categoriajogo+"\n Quantidade de ingressos: "+quantingresso+ " ingresos \n --- Valores --- \n Valor do ingresso:" +valor+ "\n Valor do total ingresso: "+totalingresso)
+}
+else if (tipojogo.toLocaleLowerCase()=="do" && etapajogo.toLocaleLowerCase()=="sf" && categoriajogo == 2)
+{
+  valor2 = 880*quantingresso
+  let totalingresso = valor2.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+  console.log( " Nome do cliente: "+nomecompleto +"\n Tipo do jogo: "+tipojogo+ "\n Etapa do Jogo: "+etapajogo+"\n Categoria: "+categoriajogo+"\n Quantidade de ingressos: "+quantingresso+ "\n Valor do total ingresso: "+totalingresso)
+}
+else if (tipojogo.toLocaleLowerCase()=="do" && etapajogo.toLocaleLowerCase()=="sf" && categoriajogo == 3)
+{
+  valor3 = 550*quantingresso
+  let totalingresso = valor3.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+  console.log( " Nome do cliente: "+nomecompleto +"\n Tipo do jogo: "+tipojogo+ "\n Etapa do Jogo: "+etapajogo+"\n Categoria: "+categoriajogo+"\n Quantidade de ingressos: "+quantingresso+ "\n Valor do total ingresso: "+totalingresso)
+}
+else if (tipojogo.toLocaleLowerCase()=="do" && etapajogo.toLocaleLowerCase()=="sf" && categoriajogo == 4)
+{
+  valor4 = 220*quantingresso
+  let totalingresso = valor4.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+  console.log( " Nome do cliente: "+nomecompleto +"\n Tipo do jogo: "+tipojogo+ "\n Etapa do Jogo: "+etapajogo+"\n Categoria: "+categoriajogo+"\n Quantidade de ingressos: "+quantingresso+ "\n Valor do total ingresso: "+totalingresso)
+}
+
+
+
+else if (tipojogo.toLocaleLowerCase()=="do" && etapajogo.toLocaleLowerCase()=="dt" && categoriajogo == 1)
+{
+  valor1 = 660*quantingresso
+  let totalingresso = valor1.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+  console.log( " Nome do cliente: "+nomecompleto +"\n Tipo do jogo: "+tipojogo+ "\n Etapa do Jogo: "+etapajogo+"\n Categoria: "+categoriajogo+"\n Quantidade de ingressos: "+quantingresso+ "\n Valor do total ingresso: "+totalingresso)
+}
+else if (tipojogo.toLocaleLowerCase()=="do" && etapajogo.toLocaleLowerCase()=="dt" && categoriajogo == 2)
+{
+  valor2 = 440*quantingresso
+  let totalingresso = valor2.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+  console.log( " Nome do cliente: "+nomecompleto +"\n Tipo do jogo: "+tipojogo+ "\n Etapa do Jogo: "+etapajogo+"\n Categoria: "+categoriajogo+"\n Quantidade de ingressos: "+quantingresso+ "\n Valor do total ingresso: "+totalingresso)
+}
+else if (tipojogo.toLocaleLowerCase()=="do" && etapajogo.toLocaleLowerCase()=="dt" && categoriajogo == 3)
+{
+  valor3 = 330*quantingresso
+  let totalingresso = valor3.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+  console.log( " Nome do cliente: "+nomecompleto +"\n Tipo do jogo: "+tipojogo+ "\n Etapa do Jogo: "+etapajogo+"\n Categoria: "+categoriajogo+"\n Quantidade de ingressos: "+quantingresso+ "\n Valor do total ingresso: "+totalingresso)
+}
+else if (tipojogo.toLocaleLowerCase()=="do" && etapajogo.toLocaleLowerCase()=="dt" && categoriajogo == 4)
+{
+  valor4 = 170*quantingresso
+  let totalingresso = valor4.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+  console.log( " Nome do cliente: "+nomecompleto +"\n Tipo do jogo: "+tipojogo+ "\n Etapa do Jogo: "+etapajogo+"\n Categoria: "+categoriajogo+"\n Quantidade de ingressos: "+quantingresso+ "\n Valor do total ingresso: "+totalingresso)
+}
+
+
+else if (tipojogo.toLocaleLowerCase()=="do" && etapajogo.toLocaleLowerCase()=="fi" && categoriajogo == 1)
+{
+  valor1 = 1980*quantingresso
+  let totalingresso = valor1.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+  console.log( " Nome do cliente: "+nomecompleto +"\n Tipo do jogo: "+tipojogo+ "\n Etapa do Jogo: "+etapajogo+"\n Categoria: "+categoriajogo+"\n Quantidade de ingressos: "+quantingresso+ "\n Valor do total ingresso: "+totalingresso)
+}
+else if (tipojogo.toLocaleLowerCase()=="do" && etapajogo.toLocaleLowerCase()=="fi" && categoriajogo == 2)
+{
+  valor2 = 1320*quantingresso
+  let totalingresso = valor2.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+  console.log( " Nome do cliente: "+nomecompleto +"\n Tipo do jogo: "+tipojogo+ "\n Etapa do Jogo: "+etapajogo+"\n Categoria: "+categoriajogo+"\n Quantidade de ingressos: "+quantingresso+ "\n Valor do total ingresso: "+totalingresso)
+}
+
+else if (tipojogo.toLocaleLowerCase()=="do" && etapajogo.toLocaleLowerCase()=="fi" && categoriajogo == 3)
+{
+  valor3 = 880*quantingresso
+  let totalingresso = valor3.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+  console.log( " Nome do cliente: "+nomecompleto +"\n Tipo do jogo: "+tipojogo+ "\n Etapa do Jogo: "+etapajogo+"\n Categoria: "+categoriajogo+"\n Quantidade de ingressos: "+quantingresso+ "\n Valor do total ingresso: "+totalingresso)
+}
+
+else if (tipojogo.toLocaleLowerCase()=="do" && etapajogo.toLocaleLowerCase()=="fi" && categoriajogo == 4)
+{
+  valor4 = 330*quantingresso
+  let totalingresso = valor4.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+  console.log( " Nome do cliente: "+nomecompleto +"\n Tipo do jogo: "+tipojogo+ "\n Etapa do Jogo: "+etapajogo+"\n Categoria: "+categoriajogo+"\n Quantidade de ingressos: "+quantingresso+ "\n Valor do total ingresso: "+totalingresso)
+}
+ 
+//INTERNACIONAL
+else if (tipojogo.toLocaleLowerCase()=="in" && etapajogo.toLocaleLowerCase()=="sf" && categoriajogo == 1)
+{
+  valor1 = 321,95*quantingresso
+  let totalingresso = valor1.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+  console.log( " Nome do cliente: "+nomecompleto +"\n Tipo do jogo: "+tipojogo+ "\n Etapa do Jogo: "+etapajogo+"\n Categoria: "+categoriajogo+"\n Quantidade de ingressos: "+quantingresso+ "\n Valor do total ingresso: "+totalingressoin)
+}
+else if (tipojogo.toLocaleLowerCase()=="in" && etapajogo.toLocaleLowerCase()=="sf" && categoriajogo == 2)
+{
+  valor2 = 214,63*quantingresso
+  let totalingresso = valor2.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+  console.log( " Nome do cliente: "+nomecompleto +"\n Tipo do jogo: "+tipojogo+ "\n Etapa do Jogo: "+etapajogo+"\n Categoria: "+categoriajogo+"\n Quantidade de ingressos: "+quantingresso+ "\n Valor do total ingresso: "+totalingressoin)
+}
+else if (tipojogo.toLocaleLowerCase()=="in" && etapajogo.toLocaleLowerCase()=="sf" && categoriajogo == 3)
+{
+  valor3 = 134,14*quantingresso
+  let totalingresso = valor3.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+  console.log( " Nome do cliente: "+nomecompleto +"\n Tipo do jogo: "+tipojogo+ "\n Etapa do Jogo: "+etapajogo+"\n Categoria: "+categoriajogo+"\n Quantidade de ingressos: "+quantingresso+ "\n Valor do total ingresso: "+totalingressoin)
+}
+else if (tipojogo.toLocaleLowerCase()=="in" && etapajogo.toLocaleLowerCase()=="sf" && categoriajogo == 4)
+{
+  valor4 = 53,65*quantingresso
+  let totalingresso = valor4.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+  console.log( " Nome do cliente: "+nomecompleto +"\n Tipo do jogo: "+tipojogo+ "\n Etapa do Jogo: "+etapajogo+"\n Categoria: "+categoriajogo+"\n Quantidade de ingressos: "+quantingresso+ "\n Valor do total ingresso: "+totalingressoin)
+}
+
+
+
+else if (tipojogo.toLocaleLowerCase()=="in" && etapajogo.toLocaleLowerCase()=="dt" && categoriajogo == 1)
+{
+  valor1 = 160,97*quantingresso
+  let totalingresso = valor1.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+  console.log( " Nome do cliente: "+nomecompleto +"\n Tipo do jogo: "+tipojogo+ "\n Etapa do Jogo: "+etapajogo+"\n Categoria: "+categoriajogo+"\n Quantidade de ingressos: "+quantingresso+ "\n Valor do total ingresso: "+totalingressoin)
+  
+}
+else if (tipojogo.toLocaleLowerCase()=="in" && etapajogo.toLocaleLowerCase()=="dt" && categoriajogo == 2)
+{
+  valor2 = 107,31*quantingresso
+  let totalingresso = valor2.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+  console.log( " Nome do cliente: "+nomecompleto +"\n Tipo do jogo: "+tipojogo+ "\n Etapa do Jogo: "+etapajogo+"\n Categoria: "+categoriajogo+"\n Quantidade de ingressos: "+quantingresso+ "\n Valor do total ingresso: "+totalingressoin)
+  
+}
+else if (tipojogo.toLocaleLowerCase()=="in" && etapajogo.toLocaleLowerCase()=="dt" && categoriajogo == 3)
+{
+  valor3 = 80,48*quantingresso
+  let totalingresso = valor3.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+  console.log( " Nome do cliente: "+nomecompleto +"\n Tipo do jogo: "+tipojogo+ "\n Etapa do Jogo: "+etapajogo+"\n Categoria: "+categoriajogo+"\n Quantidade de ingressos: "+quantingresso+ "\n Valor do total ingresso: "+totalingressoin)
+ 
+}
+else if (tipojogo.toLocaleLowerCase()=="in" && etapajogo.toLocaleLowerCase()=="dt" && categoriajogo == 4)
+{
+  valor4 =  41,46*quantingresso
+  let totalingresso = valor4.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+  console.log( " Nome do cliente: "+nomecompleto +"\n Tipo do jogo: "+tipojogo+ "\n Etapa do Jogo: "+etapajogo+"\n Categoria: "+categoriajogo+"\n Quantidade de ingressos: "+quantingresso+ "\n Valor do total ingresso: "+totalingressoin)
+}
+
+
+else if (tipojogo.toLocaleLowerCase()=="in" && etapajogo.toLocaleLowerCase()=="fi" && categoriajogo == 1)
+{
+  valor1 = 482,92*quantingresso
+  let totalingresso = valor1.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+  console.log( " Nome do cliente: "+nomecompleto +"\n Tipo do jogo: "+tipojogo+ "\n Etapa do Jogo: "+etapajogo+"\n Categoria: "+categoriajogo+"\n Quantidade de ingressos: "+quantingresso+ "\n Valor do total ingresso: "+totalingressoin)
+  
+}
+else if (tipojogo.toLocaleLowerCase()=="in" && etapajogo.toLocaleLowerCase()=="fi" && categoriajogo == 2)
+{
+  valor2 = 321,95*quantingresso
+  let totalingresso = valor2.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+  console.log( " Nome do cliente: "+nomecompleto +"\n Tipo do jogo: "+tipojogo+ "\n Etapa do Jogo: "+etapajogo+"\n Categoria: "+categoriajogo+"\n Quantidade de ingressos: "+quantingresso+ "\n Valor do total ingresso: "+totalingressoin)
+  
+}
+else if (tipojogo.toLocaleLowerCase()=="in" && etapajogo.toLocaleLowerCase()=="fi" && categoriajogo == 3)
+{
+  valor3 = 214,63*quantingresso
+  let totalingresso = valor3.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+  console.log( " Nome do cliente: "+nomecompleto +"\n Tipo do jogo: "+tipojogo+ "\n Etapa do Jogo: "+etapajogo+"\n Categoria: "+categoriajogo+"\n Quantidade de ingressos: "+quantingresso+ "\n Valor do total ingresso: "+totalingressoin)
+  
+}
+else if (tipojogo.toLocaleLowerCase()=="in" && etapajogo.toLocaleLowerCase()=="fi" && categoriajogo == 4)
+{
+  valor4 = 80,48*quantingresso
+  let totalingresso = valor4.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+  console.log( " Nome do cliente: "+nomecompleto +"\n Tipo do jogo: "+tipojogo+ "\n Etapa do Jogo: "+etapajogo+"\n Categoria: "+categoriajogo+"\n Quantidade de ingressos: "+quantingresso+ "\n Valor do total ingresso: "+totalingressoin)
+}
+
+
+
+
+
+
+
+
+
 
 
 
