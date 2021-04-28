27b0bf06f21f3e97be3a39c5c8981c2979a3574a
unknown <Administrador@CETEM-2580.mineral.cetem>
2021-04-28 18:40:20 -0300

    Exercício finalizado sem o desafio

--- semana4/objetos/aula16-objetos/blog/index.js
+++ semana4/objetos/aula16-objetos/blog/index.js
@@ -1,14 +1,33 @@
-function blog(){
-    const titulo = document.getElementById ("titulo-post").value
-    const autor = document.getElementById ("autor-post").value
-    const conteudo = document.getElementById("conteudo-post").value
+function blog(){               
+      
+        const conteudoBlog = document.getElementById("conteudoblog")
+        const img = document.getElementById ("img-post")
+        const titulo = document.getElementById ("titulo-post")
+        const autor = document.getElementById ("autor-post")
+        const conteudo = document.getElementById("conteudo-post")
 
-    titulo.innerHTML += `<p>${titulo}</p>`
-    autor.innerHTML += `<p>${autor}</p>`
-    conteudo.innerHTML += `<p>${conteudo}</p>`
+        if (titulo.value==="" && autor.value ==="" && conteudo.value==="") {
+            alert("Campos em branco, por favor insira as informações")
+          } 
+          else {  
 
-    console.log(titulo)
-    console.log(autor)
-    console.log(conteudo)
+        const post = {
+            Titulo: titulo.value,
+            Autor: autor.value,
+            Conteudo:conteudo.value,
+        }   
+        
+        const ArrayBlogConteudo=[]
+        ArrayBlogConteudo.push(post)
+         
 
+        conteudoBlog.innerHTML += `<h3>Titulo:${post.Titulo}</h3>`
+        conteudoBlog.innerHTML += `<h3>Autor(a):${post.Autor}</h3>`
+        conteudoBlog.innerHTML += `<h4>Conteúdo:${post.Conteudo}</h4>`
+
+    }
+        img.value=""
+        titulo.value=""
+        autor.value=""
+        conteudo.value=""
 }
