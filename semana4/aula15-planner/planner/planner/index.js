4ac90e7d1c398a359ed6c1f11c89b436a662d9f8
unknown <Administrador@CETEM-2580.mineral.cetem>
2021-04-27 21:46:50 -0300

    Exercicio finalizado sem o desafio

--- semana4/aula15-planner/planner/planner/index.js
+++ semana4/aula15-planner/planner/planner/index.js
@@ -1,37 +1,45 @@
-function planner (){
-const mytarefa = document.getElementById ('tarefa')
-const mydia = document.getElementById ('dias-semana')
-    if (mytarefa.value === ""){ 
- }
- else { 
-    switch (mydia.value){
-       case "domindo":
-         domingo.innerHTML += `<lu>${tarefa.value}</lu>`
+function planner (){ //chamando essa função pega os elementos
+const mytarefa = document.getElementById ('tarefa') //amarzena o elemento com id 'tarefa' na variavel mytarefa
+const mydia = document.getElementById ('dias-semana')//armazena o elemento com id 'dias-semana' na variavel mydia
+   if (mytarefa.value !== ""){ // Se o valor for diferente de vazio no input, realizar os comandos abaixo
+      switch (mydia.value){
+       case "domingo": // caso seja selecionado a opção domingo, será executado os comandos abaixo até o primeiro break
+         const domingo = document.getElementById ('domingo') //pega o elemento no id domingo e armazena na variavel domingo
+         domingo.innerHTML += `<li>${mytarefa.value}</li>`// o elemento em HTML recebe o valor armazenado em mytarefa
         break
         case "segunda":
-        segunda.innerHTML += `<li>${tarefa.value}</li>`
+         const segunda = document.getElementById ('segunda')
+         segunda.innerHTML += `<li>${mytarefa.value}</li>`
         break
         case "terca":
-        terca.innerHTML += `<li>${tarefa.value}</li>`
+         const terca = document.getElementById ('terca')
+         terca.innerHTML += `<li>${mytarefa.value}</li>`
         break
         case "quarta":
-        quarta.innerHTML += `<li>${tarefa.value}</li>`
+         const quarta = document.getElementById ('quarta')
+         quarta.innerHTML += `<li>${mytarefa.value}</li>`
         break
         case "quinta":
-        quinta.innerHTML += `<li>${tarefa.value}</li>`
+         const quinta = document.getElementById ('quinta')   
+         quinta.innerHTML += `<li>${mytarefa.value}</li>`
         break
         case "sexta":
-        sexta.innerHTML += `<li>${tarefa.value}</li>`
+         const sexta = document.getElementById ('sexta')
+         sexta.innerHTML += `<li>${mytarefa.value}</li>`
         break
         case "sabado":
-        sabado.innerHTML += `<li>${tarefa.value}</li>`
-        break
-        
-        break;
-        
+        const sabado = document.getElementById ('sabado')
+        sabado.innerHTML += `<li>${mytarefa.value}</li>`
+        break       
+                
       }
 
-   
+      mytarefa.value = "" //Input fica vazio
    }
-   mytarefa.value = ""
+   
 }
+
+
+   
+   
+
