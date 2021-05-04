2c6c52ae2764c0028b047d61c2db9d8245dc734f
unknown <Administrador@CETEM-2580.mineral.cetem>
2021-05-04 19:51:40 -0300

    Finalizado

--- semana5/componentes_aderecos/labedin/src/App.js
+++ semana5/componentes_aderecos/labedin/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
 import CardGrande from './components/CardGrande/CardGrande';
+import CardPequeno from './components/CardPequeno/CardPequeno';
 import ImagemButton from './components/ImagemButton/ImagemButton';
-import PerfilImg from './components/';
+import PerfilImg from './img/Fabi.jpg';
+import Profissao from './img/ibrowse.png';
+import EmailContato from './img/email.png';
+import Local from './img/local.png';
+
 
 function App() {
   return (
@@ -10,7 +15,7 @@ function App() {
       <div className="page-section-container">
         <h2>Dados pessoais</h2>
         <CardGrande 
-          imagem={Fabi}
+          imagem={PerfilImg} 
           nome="Fabricia Sant Anna" 
           descricao="Oi, eu sou Fabricia Sant Anna. Sou estudante Web FullStack da Labenu e Supervisora de Service Desk composto por um time de 7 pessoas. Adoro novos desafios e minha meta é a transição profissional para a área de desenvolvimento."
         />
@@ -20,19 +25,34 @@ function App() {
           texto="Ver mais"
         />
       </div>
+      <div className="page-section-container">
+        <CardPequeno
+        imagem ={EmailContato}
+        nome="Email:"
+        descricao="briciasilv@gmail.com"
+        />
+      </div> 
+
+      <div className="page-section-container">
+        <CardPequeno
+        imagem ={Local}
+        nome="Endereço:"
+        descricao="Praia da Guanabara - Ilha do Governador"
+        />
+      </div> 
 
       <div className="page-section-container">
         <h2>Experiências profissionais</h2>
         <CardGrande 
-          imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
-          nome="Labenu" 
-          descricao="Formando desenvolvedores para o mercado de trabalho!" 
+          imagem={Profissao}
+          nome="Ibrowse - CETEM" 
+          descricao="Supervisora de Service Desk durante 3 anos." 
         />
         
         <CardGrande 
-          imagem="https://imagens.canaltech.com.br/empresas/4418.400.jpg" 
-          nome="NASA" 
-          descricao="Apontando defeitos." 
+          imagem={Profissao} 
+          nome="Ibrowse-INPI" 
+          descricao="Suporte de TI de Nível 2 para usuários durante 2 anos." 
         />
       </div>
 
@@ -46,8 +66,9 @@ function App() {
         <ImagemButton 
           imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" 
           texto="Twitter" 
-        />        
-      </div>
+        />            
+      </div>    
+       
     </div>
   );
 }
