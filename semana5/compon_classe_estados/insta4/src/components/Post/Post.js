3e2aba15f1457f57ed4bad00c370dcc270ea40dd
unknown <Administrador@CETEM-2580.mineral.cetem>
2021-05-05 16:12:10 -0300

    Quase finalizando

---
+++ semana5/compon_classe_estados/insta4/src/components/Post/Post.js
@@ -0,0 +1,113 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import {IconeComContador} from '../IconeComContador/IconeComContador'
+
+import iconeCoracaoBranco from '../../img/favorite-white.svg'
+import iconeCoracaoPreto from '../../img/favorite.svg'
+import iconeComentario from '../../img/comment_icon.svg'
+import {SecaoComentario} from '../SecaoComentario/SecaoComentario' 
+//Não lembro o que significa as {SecaoComentario} 
+
+// É o css?
+const PostContainer = styled.div` 
+  border: 1px solid gray;
+  width: 300px;
+  margin-bottom: 10px;
+`
+
+const PostHeader = styled.div`
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding-left: 10px;
+`
+
+const PostFooter = styled.div`
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  justify-content: space-between;
+`
+
+const UserPhoto = styled.img`
+  height: 30px;
+  width: 30px;
+  margin-right: 10px;
+  border-radius: 50%;
+`
+
+const PostPhoto = styled.img`
+  width: 100%;
+`
+
+class Post extends React.Component { //Não lembro do significado dessa linha
+  state = {
+    curtido: false,
+    numeroCurtidas: 0,
+    comentando: false,
+    numeroComentarios: 0
+  }
+
+  onClickCurtida = () => {
+    console.log('Curtiu!')
+  }
+
+  onClickComentario = () => {
+    this.setState({
+      comentando: !this.state.comentando // Muda seu status para diferente do seu status inicial
+    })
+  }
+
+  aoEnviarComentario = () => {
+    this.setState({
+      comentando: false,
+      numeroComentarios: this.state.numeroComentarios + 1
+    })
+  }
+
+  render() {
+    let iconeCurtida
+
+    if(this.state.curtido) {
+      iconeCurtida = iconeCoracaoPreto
+    } else {
+      iconeCurtida = iconeCoracaoBranco
+    }
+
+    let componenteComentario
+
+    if(this.state.comentando) {
+      componenteComentario = <SecaoComentario aoEnviar={this.aoEnviarComentario}/>
+    } // Não entendi a linha 81
+
+    // Entender melhor a linha87
+    //Linha 96 - Icone, onclickIcone e valorContador, recebem valores em curtidas, todos veêm de IconeComContador e vão para o Footer
+    return <PostContainer> 
+      <PostHeader>
+        <UserPhoto src={this.props.fotoUsuario} alt={'Imagem do usuario'}/>
+        <p>{this.props.nomeUsuario}</p> 
+      </PostHeader>
+
+      <PostPhoto src={this.props.fotoPost} alt={'Imagem do post'}/>
+      
+      <PostFooter>
+        <IconeComContador
+          icone={iconeCurtida}
+          onClickIcone={this.onClickCurtida}
+          valorContador={this.state.numeroCurtidas}
+        /> 
+        
+        <IconeComContador
+          icone={iconeComentario}
+          onClickIcone={this.onClickComentario}
+          valorContador={this.state.numeroComentarios}
+        />
+      </PostFooter>
+      {componenteComentario}
+    </PostContainer>
+  }
+}
+
+export default Post // Não lembro o significado
